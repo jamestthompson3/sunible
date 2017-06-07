4f25beb96d4e7a305b1d1cf759e8c8e8f6429758
jamestthompson3 <jamestthompson3@gmail.com>
2017-06-07 11:33:49 -0700

    working on whitenoise issue

--- sunible/local.py
+++ sunible/local.py
@@ -19,7 +19,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 HOME_DIR = os.path.join(BASE_DIR,"home/templates")
 STATIC_DIR = os.path.join(BASE_DIR,"static")
 MEDIA_DIR = os.path.join(BASE_DIR,"media")
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 ROOT_URLCONF = 'sunible.urls'
 
 
