c8ffa7a97b6ca99beebc16cd9d78eb2b6037c7c3
jamestthompson3 <jamestthompson3@gmail.com>
2017-06-08 16:32:39 -0700

    added urls to redirect to blog for Rosana's presentation

--- home/urls.py
+++ home/urls.py
@@ -8,6 +8,9 @@ urlpatterns=[
 	url(r'^populateinstallerlist/$',views.populateInstallerList,name='populateinstallerlist'),
 	url(r'^privacy/$',views.privacy,name='privacy'),
 	url(r'^tos/$',views.tos,name='tos'),
-	url(r'^blog',views.blog,name='blog')
+	url(r'^blog/$',views.blog,name='blog'),
+	url(r'^blog/solar-101',RedirectView.as_view(url="http://blog.sunible.com/solar-101/")),
+	url(r'^blog/how-to-compare-solar-quotes',RedirectView.as_view(url="http://blog.sunible.com/how-to-compare-solar-quotes")),
+	url(r'^blog/solar-tax-credit-itc',RedirectView.as_view(url="http://blog.sunible.com/solar-tax-credit-itc"))
 	# url(r'^populate/$',views.populate,name='populate'),
 ]
